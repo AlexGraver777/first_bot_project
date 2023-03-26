9158d8cedca80384fc5537491bc5d8a3802e7c1c
Alex Graver <graver.graver.80@gmail.com>
2023-03-26 20:26:53 +0300

    TIK 0001: Add message function

--- bot.py
+++ bot.py
@@ -1,25 +1,31 @@
-import sys
-from os import environ
+# import sys
+# from os import environ
+#
+# # noinspection PyPackageRequirements
+# import telebot
+#
+# BOT_TOKEN = environ.get("BOT_TOKEN")
+#
+# if BOT_TOKEN is None:
+#     sys.exit("Set BOT_TOKEN in environment variables")
+#
+# bot = telebot.TeleBot(BOT_TOKEN)
+#
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
+#
+#
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+#
+#
+# bot.infinity_polling()
 
-# noinspection PyPackageRequirements
-import telebot
 
-BOT_TOKEN = environ.get("BOT_TOKEN")
+def message_for_client(*args, **kwargs):
+    print('Вы всегда можете решить все вопросы, позвонив менеджеру')
 
-if BOT_TOKEN is None:
-    sys.exit("Set BOT_TOKEN in environment variables")
-
-bot = telebot.TeleBot(BOT_TOKEN)
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+message_for_client()
