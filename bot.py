c5554c45f43a8053f8526ab8f50010785361a256
Alex Graver <graver.graver.80@gmail.com>
2023-03-27 15:08:59 +0300

    Add greet txt function

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, """Добрый день! Это бот компании 'Бери Рули' Томск. Здесь вы сможете сделать предварительный заказ
+    авто, оставить свои пожелания, оставить заявку для связи с менеджером.""")
 
 
 @bot.message_handler(func=lambda message: True)
